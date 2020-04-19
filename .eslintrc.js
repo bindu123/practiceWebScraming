3a854116042b82a6bbe85acd6866f5c78572212c
Bindu <bindurani390@gmail.com>
2020-04-19 17:19:52 +0530

    add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "editor.codeActionsOnSave": {
+        "source.fixAll.eslint": true
+    },
+    "editor.codeActionsOnSave": {
+        "source.fixAll": true
+    },
+    "rules": {
+        "prefer-arrow-callback": "error"
+    }
+};
